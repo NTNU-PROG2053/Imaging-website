d64e700d64908e2c8904c3f59176a57e525d5cf9
gluppe <magnujg@stud.ntnu.no>
2021-10-22 16:43:08 +0200

    testing list item text

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -15,22 +15,28 @@ class UserDetail extends React.Component {
 
   render() {
     return (
-      <Typography variant="body1">
+      
+      
         <div id="User_details">
+          <List>
+            <ListItem>
+              <ListItemText primary="First Name: " secondary={this.props.match.params.first_name}></ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="">
+              </ListItemText>
+            </ListItem>
+
+          </List>
           <ul>
-            <li>First name: {this.props.match.params.first_name} </li>
+            <li>{this.props.match.params.first_name} </li>
             <li>Last name: {this.props.match.params.last_name}</li>
             <li>Location: {this.props.match.params.location}</li>
             <li>Description: {this.props.match.params.description}</li>
             <li>Occupation: {this.props.match.param.occupation}</li>
           </ul>
         </div>
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.cs142models.userModel(userId).
-      </Typography>
+      
     );
   }
 }
