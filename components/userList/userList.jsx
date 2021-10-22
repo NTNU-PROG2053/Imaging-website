befe76c4987c88f6ca72be7dac6b6ef664bc85a2
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-10-22 16:25:27 +0200

    Users now have a link

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -7,6 +7,9 @@ import {
   Typography,
 }
   from '@material-ui/core';
+import {
+  Link
+} from 'react-router-dom';
 import './userList.css';
 
 /**
@@ -25,26 +28,17 @@ class UserList extends React.Component {
   render() {
     return (
       <div>
-        <p></p>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://material-ui.com/demos/lists/">Lists</a> and <a href="https://material-ui.com/demos/dividers">Dividers</a> to
-          display your users like so:
-        </Typography>
         <List component="nav">
           <Divider />
           {this.state.userList.map((user) =>
             <div key={user._id}>
-              <ListItem>
+              <ListItem button component={Link} to={`/users/${user._id}`}>
                 <ListItemText primary={user.first_name + " " + user.last_name} />
               </ListItem>
               <Divider />
             </div>
           )}
         </List>
-        <Typography variant="body1">
-          The model comes in from window.cs142models.userListModel()
-        </Typography>
       </div>
     );
   }
