5859becf58fce2d7837eea6d9b331e1fb54b8401
gluppe <magnujg@stud.ntnu.no>
2021-10-22 16:21:15 +0200

    First commit for branch

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -16,6 +16,15 @@ class UserDetail extends React.Component {
   render() {
     return (
       <Typography variant="body1">
+        <div id="User_details">
+          <ul>
+            <li>First name: {this.props.match.params.first_name} </li>
+            <li>Last name: {this.props.match.params.last_name}</li>
+            <li>Location: {this.props.match.params.location}</li>
+            <li>Description: {this.props.match.params.description}</li>
+            <li>Occupation: {this.props.match.param.occupation}</li>
+          </ul>
+        </div>
         This should be the UserDetail view of the PhotoShare app. Since
         it is invoked from React Router the params from the route will be
         in property match. So this should show details of user:
@@ -25,5 +34,13 @@ class UserDetail extends React.Component {
     );
   }
 }
+
+/*_id  (string) - The ID of the user.
+   first_name (string) - The first name of the user.
+   last_name (string) - The last name of the user.
+   location (string) - The location of the user.
+   description (string) - A brief user description.
+   occupation (string) - The occupation of the user.
+   */
 
 export default UserDetail;
