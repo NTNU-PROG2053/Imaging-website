e3d5b532873723361044a8c5505841af777af4f4
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-10-22 16:17:16 +0200

    Left side shows user names

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -6,7 +6,7 @@ import {
   ListItemText,
   Typography,
 }
-from '@material-ui/core';
+  from '@material-ui/core';
 import './userList.css';
 
 /**
@@ -15,29 +15,32 @@ import './userList.css';
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      userList: window.cs142models.userListModel()
+    };
+
   }
 
   render() {
     return (
       <div>
+        <p></p>
         <Typography variant="body1">
           This is the user list, which takes up 3/12 of the window.
           You might choose to use <a href="https://material-ui.com/demos/lists/">Lists</a> and <a href="https://material-ui.com/demos/dividers">Dividers</a> to
           display your users like so:
         </Typography>
         <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
           <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+          {this.state.userList.map((user) =>
+            <div key={user._id}>
+              <ListItem>
+                <ListItemText primary={user.first_name + " " + user.last_name} />
+              </ListItem>
+              <Divider />
+            </div>
+          )}
         </List>
         <Typography variant="body1">
           The model comes in from window.cs142models.userListModel()
