5866e61469b87d04dca26a3c6e3a37bbbef6ab7f
gluppe <magnujg@stud.ntnu.no>
2021-10-23 14:44:56 +0200

    added user data in the fields in userdetail

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -4,6 +4,7 @@ import {
   List,
   ListItem,
   ListItemText,
+  HashRouter,
 } from '@material-ui/core';
 import './userDetail.css';
 
@@ -15,24 +16,38 @@ class UserDetail extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      userModel: window.cs142models.userModel(this.props.match.params.userId)
+      
+    };
+
   }
 
   render() {
     return (
       
-      
         <div id="User_details">
+          
+          <div>
           <List>
             <ListItem>
-              <ListItemText primary="First Name: " secondary={this.props.match.params.first_name}></ListItemText>
+              <ListItemText primary="First Name" secondary={this.state.userModel.first_name}></ListItemText>
             </ListItem>
             <ListItem>
-              <ListItemText primary="">
-              </ListItemText>
+              <ListItemText primary="Last Name" secondary={this.state.userModel.last_name}></ListItemText>
             </ListItem>
-
+            <ListItem>
+              <ListItemText primary="Location" secondary={this.state.userModel.location}></ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Description" secondary={this.state.userModel.description}></ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Occupation" secondary={this.state.userModel.occupation}></ListItemText>
+            </ListItem> 
           </List>
-    
+          </div>
+          
         </div>
       
     );
