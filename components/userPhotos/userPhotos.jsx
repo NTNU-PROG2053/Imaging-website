42088317aa9f7cbc2fe877991087cc341e41d0c4
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-05 16:01:27 +0100

    Added comment header

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -3,7 +3,7 @@ import {
   Card,
   CardMedia,
   Typography,
-  CardHeader
+  CardHeader,
 } from '@material-ui/core';
 import Comment from '../comment/Comment';
 import './userPhotos.css';
@@ -26,10 +26,11 @@ class UserPhotos extends React.Component {
           <Card key={photo._id} className="imageDiv">
             <CardHeader title={photo.file_name} subheader={photo.date_time}></CardHeader>
             <CardMedia component="img" image={`../images/${photo.file_name}`} alt={"Could not display image"} />
+            <Typography variant="h5">Comments</Typography>
             {
               photo.comments ? (
                 photo.comments.map(comment => (
-                  <Comment key={comment._id} text={comment.comment} user={comment.user} date={comment.date_time}/>
+                  <Comment  key={comment._id} text={comment.comment} user={comment.user} date={comment.date_time}/>
                 )
                 )) : <Typography>No comments yet.</Typography>
             }
