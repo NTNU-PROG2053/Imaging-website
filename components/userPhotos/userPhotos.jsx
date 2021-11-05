194824816487a7f50ba292890e7441bffd06e3c3
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-11-05 15:52:12 +0100

    Added link to user page on comments

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -29,7 +29,7 @@ class UserPhotos extends React.Component {
             {
               photo.comments ? (
                 photo.comments.map(comment => (
-                  <Comment key={comment._id} text={comment.comment} userName={comment.user.first_name + " " + comment.user.last_name} date={comment.date_time}/>
+                  <Comment key={comment._id} text={comment.comment} user={comment.user} date={comment.date_time}/>
                 )
                 )) : <Typography>No comments yet.</Typography>
             }
